6bddabf472710839147557cb48d626393d19fa36
IsraelMerlyn <merlyn142a@gmail.com>
2023-07-29 08:17:20 -0600

    levantamiento de entornor de desarrollo

---
+++ src/main/java/com/josuevqz/users/usuarioslogin/UsuariosLoginApplication.java
@@ -0,0 +1,13 @@
+package com.josuevqz.users.usuarioslogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josuevqz/users/usuarioslogin/UsuariosLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josuevqz.users.usuarioslogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
