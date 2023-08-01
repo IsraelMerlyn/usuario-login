97bfbab65feeb38a169ad0bd1880440ee258f763
IsraelMerlyn <merlyn142a@gmail.com>
2023-07-31 21:30:04 -0600

    Tasks #0.0.1.16 Implementando estructuras y metodos JPA

--- src/main/java/com/josuevqz/users/usuarioslogin/services/JPAUserDetailsServices.java
+++ src/main/java/com/josuevqz/users/usuarioslogin/services/JPAUserDetailsServices.java
@@ -1,5 +1,7 @@
 package com.josuevqz.users.usuarioslogin.services;
 
+import com.josuevqz.users.usuarioslogin.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -7,22 +9,30 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JPAUserDetailsServices implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!username.equals("admin")){
+        Optional<com.josuevqz.users.usuarioslogin.models.User> o = repository.findByUsername(username);
+        if (!o.isPresent()){
             throw  new UsernameNotFoundException(String.format("Username %s no existe en el sistema",  username ));
         }
+        com.josuevqz.users.usuarioslogin.models.User user = o.orElseThrow();
         List<GrantedAuthority>authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
 
-        return new User(username,
-                "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS",
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
                 true,
                 true,
                 true,
